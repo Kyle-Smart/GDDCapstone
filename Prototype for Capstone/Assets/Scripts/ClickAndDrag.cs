bf0b17652ceed10f6fcc2c6b611c40bb8d7ffcfc
Kyle-Smart <ksmart@uccs.edu>
2021-01-31 18:07:17 -0700

    Initial push to backup the current prototype

---
+++ Prototype for Capstone/Assets/Scripts/ClickAndDrag.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickAndDrag : MonoBehaviour
+{
+    //Variables
+    private Vector3 mOffset;    //Tracks the offset of the mouse in reference to the UI object
+    private float mOffsetZ;     //Tracks the Z coordinate of the offset (not 100% if needed for a 2D game)
+
+    //This is the game object version of the UI element
+    [SerializeField]
+    GameObject theObjectVersion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        onUI = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //This handles having the UI element be clickable
+    private void OnMouseDown()
+    {
+        mOffsetZ = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+
+        mOffset = gameObject.transform.position - GetMouseWorldPosition();
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+
+        mousePoint.z = mOffsetZ;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    //As the mouse is dragging the UI element, follow the mouse
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPosition() + mOffset;
+    }
+
+    private void OnMouseUp()
+    {
+        Instantiate(theObjectVersion, gameObject.transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ Prototype for Capstone/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Vars
+    const float maxSpeed = 40.0f;
+    const float jumpingSpeed = 100.0f;
+    float currentMovementHorizontal = 0.0f;
+    float currentMovementVertical = 0.0f;
+    float currentDirection = 0.0f;
+    float jumpingInput = 0.0f;
+    Rigidbody2D player;
+    bool grounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+        grounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerWalking();
+        PlayerJump();
+    }
+
+    private void PlayerWalking()
+    {
+        if (Input.GetAxis("Horizontal") > 0.2 || Input.GetAxis("Horizontal") < -0.2)
+        {
+            player.velocity = new Vector2(Input.GetAxis("Horizontal") * maxSpeed, player.velocity.y);
+        } else
+        {
+            player.velocity = new Vector2(0, player.velocity.y);
+        }
+    }
+    
+    private void PlayerJump()
+    {
+        if (Input.GetAxis("Jump") > 0.5 && grounded)
+        {
+            grounded = false;
+            player.velocity = Vector2.up * jumpingSpeed;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+}
