34cf970ab05327ee5a8002b75599c34b62df9c7a
Kyle Smart <ksmart@uccs.edu>
2021-03-29 16:16:56 -0500

    Added working Narrator Dialogue system

---
+++ Prototype for Capstone/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> theDialog;
+    [SerializeField]
+    Text narratorText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theDialog = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        theDialog.Clear();
+        narratorText.text = "";
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            theDialog.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (theDialog.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string nextSentence = theDialog.Dequeue();
+
+        narratorText.text = nextSentence;
+    }
+
+    public void EndDialogue()
+    {
+        narratorText.text = "";
+    }
+}
---
+++ Prototype for Capstone/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public float amountToDelayStartBy;
+
+    public void Start()
+    {
+       StartCoroutine(TriggerDialogue());
+    }
+
+    public IEnumerator TriggerDialogue()
+    {
+        yield return new WaitForSeconds(amountToDelayStartBy);
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
