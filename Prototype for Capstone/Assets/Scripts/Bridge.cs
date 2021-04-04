28c9782680c0b1780e30a31d88cf4894956825bd
Kyle-Smart <ksmart@uccs.edu>
2021-04-04 12:43:52 -0600

    Finished making Level 5 funcionality

--- Prototype for Capstone/Assets/Scripts/Bridge.cs
+++ Prototype for Capstone/Assets/Scripts/Bridge.cs
@@ -6,38 +6,60 @@ public class Bridge : MonoBehaviour
 {
     [SerializeField]
     Vector2 extendedPosition;
+    [SerializeField]
+    GameObject ButtonToPower;
+    [SerializeField]
+    GameObject ButtonToMove;
+    [SerializeField]
+    GameObject GeneratorToPowerAndMove;
 
     private Vector2 retractedPosition;
     public bool isPowered;
     public bool isExtended;
-
-    private List<PushDownButton> buttonsInLevel;
 
     // Start is called before the first frame update
     void Start()
     {
         isPowered = false;
         isExtended = false;
-        buttonsInLevel = new List<PushDownButton>();
         retractedPosition = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
-
-        foreach (GameObject aButton in GameManager.Instance.Buttons)
-            buttonsInLevel.Add(aButton.GetComponent<PushDownButton>());
     }
 
     // Update is called once per frame
     void Update()
     {
-        foreach (PushDownButton aButton in buttonsInLevel)
+        if (GeneratorToPowerAndMove == null)
         {
-            string theButtonTag = aButton.gameObject.tag;
-            if (theButtonTag == "GivesPower")
+            bool isBridgePowered = ButtonToPower.GetComponent<PushDownButton>().IsButtonPressed();
+            if (isBridgePowered)
             {
-                isPowered = aButton.IsButtonPressed();
+                isPowered = true;
             }
-            else if (theButtonTag == "StartsAction")
+            else
             {
-                isExtended = aButton.IsButtonPressed();
+                isPowered = false;
+            }
+
+            bool isBridgeExtended = ButtonToMove.GetComponent<PushDownButton>().IsButtonPressed();
+            if (isBridgeExtended)
+            {
+                isExtended = true;
+            }
+            else
+            {
+                isExtended = false;
+            }
+        } 
+        else
+        {
+            bool isGeneratorOn = GeneratorToPowerAndMove.GetComponent<Generator>().IsGeneratorOn();
+            if (isGeneratorOn)
+            {
+                isPowered = isExtended = true;
+            }
+            else
+            {
+                isPowered = isExtended = false;
             }
         }
 
