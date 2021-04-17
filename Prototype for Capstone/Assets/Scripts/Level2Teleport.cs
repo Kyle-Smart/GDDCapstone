b3f0346626b8b8ac6bf80a56aa66ead6c576aa36
Kyle-Smart <ksmart@uccs.edu>
2021-04-17 10:52:37 -0600

    Made all the levels content complete.

---
+++ Prototype for Capstone/Assets/Scripts/Level2Teleport.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level2Teleport : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.gameObject.tag == "Player") collision.transform.position = new Vector3(-8.86f, -3.6f, 0);
+    }
+}
